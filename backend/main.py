a3c9b8422b88751a2a0a79642e2e6eaa5902f798
Rachit Bhandari <rachit23413@iiitd.ac.in>
2025-05-19 20:32:20 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://smart-task-board.netlify.app"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
